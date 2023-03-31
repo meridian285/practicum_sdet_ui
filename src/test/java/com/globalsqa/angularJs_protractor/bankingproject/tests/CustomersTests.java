3c4f43ef5890b4800ad8148e6b4a99150373d71b
Alex <akelizarovav@gmail.com>
2023-03-31 21:05:51 +0300

    Добавить во все тестовые классы проверки, ранее они были в page классах

--- src/test/java/com/globalsqa/angularJs_protractor/bankingproject/tests/CustomersTests.java
+++ src/test/java/com/globalsqa/angularJs_protractor/bankingproject/tests/CustomersTests.java
@@ -3,11 +3,15 @@ package com.globalsqa.angularJs_protractor.bankingproject.tests;
 import com.globalsqa.angularJs_protractor.bankingproject.config.BaseTest;
 import com.globalsqa.angularJs_protractor.bankingproject.generator.Customer;
 import com.globalsqa.angularJs_protractor.bankingproject.generator.DataGenerator;
+import com.globalsqa.angularJs_protractor.bankingproject.pages.AddCustomerPage;
 import com.globalsqa.angularJs_protractor.bankingproject.pages.CustomersPage;
 import com.globalsqa.angularJs_protractor.bankingproject.pages.MainPage;
 import com.globalsqa.angularJs_protractor.bankingproject.pages.ManagePage;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * CustomersTests тестовый класс, для тестов связанных со страницей Customers
@@ -15,6 +19,8 @@ import org.junit.jupiter.api.Test;
 
 public class CustomersTests extends BaseTest {
     Customer customer = DataGenerator.getCustomerFaker();
+    CustomersPage customersPage = new CustomersPage();
+    AddCustomerPage addCustomerPage = new AddCustomerPage();
     MainPage mainPage = new MainPage();
     ManagePage managePage = new ManagePage() {
         @Override
@@ -32,9 +38,11 @@ public class CustomersTests extends BaseTest {
         mainPage.clickBankManagerLoginButton();
         managePage
                 .clickAddCustomerButton()
-                .createCustomer(firstName, lastName, postCode)
-                .checkMessageCreateCustomer()
-                .clickCustomersButton()
+                .createCustomer(firstName, lastName, postCode);
+        assertTrue(managePage.getTextAlertMessage().contains("Customer added successfully with customer id :"),
+                "Сообщение подтверждения создания клиента не корректно или отсутствует");
+        managePage.clickAlertAccept();
+        addCustomerPage.clickCustomersButton()
                 .findText(firstName)
                 .checkFindCustomers(firstName, lastName, postCode);
     }
@@ -45,7 +53,10 @@ public class CustomersTests extends BaseTest {
         mainPage.clickBankManagerLoginButton();
         managePage
                 .clickCustomersButton()
-                .clickOnTitleFirstName()
-                .checkSortingTable();
+                .clickOnTitleFirstName();
+        //listSortedBySite() список сортированный сайтом
+        //sortingListInDescending() тот же список сортированный в методе
+        Assertions.assertEquals(customersPage.sortingListInDescending(), customersPage.listSortedBySite(),
+                "Ожидалось совпадение списков");
     }
 }
