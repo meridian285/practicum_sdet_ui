a5417e3da7de7d9d8a1a72922932243266be001f
Alex <akelizarovav@gmail.com>
2023-03-31 21:02:03 +0300

    Добавить изменения в методы checkingThatFieldsNotFilled(), checkMessageCreateCustomerParam() удалить метод checkMessageCreateDuplicateCustomer()
    Метод checkingThatFieldsNotFilled(): убрана проверка из метода, теперь он возвращает проверяет есть ли сообщение и возвращает boolean нужное для проверки в тестовом методе
    
    В методе checkMessageCreateCustomerParam() такие же изменения

--- src/test/java/com/globalsqa/angularJs_protractor/bankingproject/pages/AddCustomerPage.java
+++ src/test/java/com/globalsqa/angularJs_protractor/bankingproject/pages/AddCustomerPage.java
@@ -1,13 +1,11 @@
 package com.globalsqa.angularJs_protractor.bankingproject.pages;
 
 import io.qameta.allure.Step;
-import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.Objects;
-import static org.junit.jupiter.api.AssertionFailureBuilder.assertionFailure;
 
 /**
  * Add Customer class
@@ -35,43 +33,20 @@ public class AddCustomerPage extends ManagePage {
     }
 
     @Step("Метод ввода значений в поля FirstName, LastName, PostCode")
-    public AddCustomerPage createCustomer(String firstname, String lastName, String postCode) {
+    public void createCustomer(String firstname, String lastName, String postCode) {
         firstNameField.sendKeys(firstname);
         lastNameField.sendKeys(lastName);
         postCodeField.sendKeys(postCode);
         addCustomerButton.click();
-        return this;
-    }
-
-    @Step("Проверка сообщения о создании клиента, после проверки жмем ОК")
-    public AddCustomerPage checkMessageCreateCustomer(){
-        String actualAlertMessage= driver.switchTo().alert().getText();
-        Assertions.assertTrue(actualAlertMessage.contains("Customer added successfully with customer id :"),
-                "Сообщение подтверждения создания клиента не корректно или отсутствует");
-        driver.switchTo().alert().accept();
-        return this;
-    }
-    @Step("Проверка сообщения о создании клиента, после проверки жмем ОК")
-    public void checkMessageCreateDuplicateCustomer(){
-        String actualAlertMessage= driver.switchTo().alert().getText();
-        Assertions.assertTrue(actualAlertMessage.contains("Please check the details. Customer may be duplicate."),
-                "Ожидалось сообщение об ошибке, нельзя создать дубликат");
-        driver.switchTo().alert().accept();
     }
 
     @Step("Проверка сообщения о создании клиента, после проверки жмем ОК. Для параметризованного теста")
-    public void checkMessageCreateCustomerParam(boolean condition) {
-        boolean actualAlertMessage= driver.switchTo().alert().getText().contains("Customer added successfully with customer id :");
-        if(actualAlertMessage != condition){
-            assertionFailure().message("Сообщение подтверждения создания клиента не корректно или отсутствует")
-                    .expected(true).actual(false).buildAndThrow();
-        }
-        driver.switchTo().alert().accept();
+    public boolean checkMessageCreateCustomerParam() {
+        return driver.switchTo().alert().getText().contains("Customer added successfully with customer id :");
     }
     @Step("Проверка что одно из полей не заполнено")
-    public void checkingThatFieldsNotFilled(){
-        boolean check = Objects.requireNonNull(formFields.getAttribute("class"))
+    public boolean checkingThatFieldsNotFilled(){
+        return Objects.requireNonNull(formFields.getAttribute("class"))
                 .contains("ng-invalid ng-invalid-required");
-        Assertions.assertTrue(check, "Свойство класса локатора не совпадает с ожидаемым");
     }
 }
